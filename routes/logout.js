88fbedea4eb8afbffca86b3a9f356093efe64ea0
siran-1 <siranjeevik21@gmail.com>
2023-11-11 23:42:21 +0000

    Changed the route of Logout from GET to POST

--- routes/logout.js
+++ routes/logout.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     req.session.destroy((error) => {
         if (error) {
             console.error("Error destroying session:", error);
